57bf143c9c349cf9ff94f887b3bb841686873ed7
Kevin Rousseau <rousseau.kevin15@gmail.com>
2023-07-15 14:13:39 +0200

    Add 3D logo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,29 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss'],
-  app: {
-    head: {
-      charset: 'utf-8',
-      viewport: 'width=device-width, initial-scale=1',
-    }
-  }
-})
+	devtools: { enabled: true },
+	modules: ["@nuxtjs/tailwindcss"],
+	app: {
+		head: {
+			title: "Kevin Rousseau | Full-stack Developer",
+			charset: "utf-8",
+			viewport: "width=device-width, initial-scale=1",
+			htmlAttrs: {
+				lang: "en",
+			},
+			link: [
+				{
+					rel: "canonical",
+					href: "https://kevinrousseau.be",
+				},
+				{ rel: "icon", type: "image/x-icon", href: "favicon.png" },
+			],
+			script: [
+				{
+					hid: "spline",
+					type: "module",
+					src: "https://unpkg.com/@splinetool/viewer@0.9.383/build/spline-viewer.js",
+				},
+			],
+		},
+	},
+});
--- pages/index.vue
+++ pages/index.vue
@@ -1,83 +1,73 @@
 <script setup lang="ts">
 const appConfig = useAppConfig();
-
-const logo = ref();
-const multiple = 30;
-
-const mouseMove = (e: MouseEvent) => {
-  console.log(e);
-  window.requestAnimationFrame(function () {
-    transformElement(e.clientX, e.clientY);
-  });
-};
-
-const mouseLeave = () => {
-  window.requestAnimationFrame(function () {
-    logo.value.style.transform = "rotateX(0) rotateY(0)";
-  });
-};
-
-const transformElement = (x: number, y: number) => {
-  let box = logo.value.getBoundingClientRect();
-  let calcX = -(y - box.y - box.height / 2) / multiple;
-  let calcY = (x - box.x - box.width / 2) / multiple;
-
-  logo.value.style.transform =
-    "rotateX(" + calcX + "deg) " + "rotateY(" + calcY + "deg)";
-};
+useSeoMeta({
+	description:
+		"I'm Kevin, a Full-stack Developer and Laravel / Vue.js enthusiast from Belgium. Don't hesitate to reach out and let's have a chat. Looking forward to hearing from you!",
+	ogDescription:
+		"I'm Kevin, a Full-stack Developer and Laravel / Vue.js enthusiast from Belgium. Don't hesitate to reach out and let's have a chat. Looking forward to hearing from you!",
+});
 </script>
 <template>
-  <div
-    class="bg-gradient-stripped w-screen min-h-screen flex items-center"
-    @mousemove="mouseMove"
-    @mouseleave="mouseLeave"
-  >
-    <div
-      class="container mx-auto z-10 mt-10 mb-48 flex items-center justify-between flex-col xl:flex-row"
-    >
-      <header class="text-white xl:w-7/12 order-2 xl:order-1 text-justify">
-        <h1 class="text-7xl lg:text-9xl font-bold opacity-50">Welcome</h1>
-        <h2
-          class="text-3xl lg:text-5xl font-bold -mt-7 pl-5 lg:-mt-10 lg:pl-10"
-        >
-          Nice to meet you
-        </h2>
+	<div class="bg-gradient-stripped w-screen min-h-screen flex items-center">
+		<div
+			class="container mx-auto z-10 mt-10 mb-48 flex justify-between flex-col xl:flex-row"
+		>
+			<header
+				class="text-white xl:w-7/12 order-2 xl:order-1 text-justify"
+			>
+				<h1 class="text-7xl lg:text-9xl font-bold opacity-50">
+					Welcome
+				</h1>
+				<h2
+					class="text-3xl lg:text-5xl font-bold -mt-7 pl-5 lg:-mt-10 lg:pl-10"
+				>
+					Nice to meet you
+				</h2>
 
-        <p class="text-md lg:text-lg lg:ml-5 mt-10 text-balance leading-loose">
-          I'm Kevin, a <strong>Full-stack Developer</strong> and
-          <strong>Laravel / Vue.js</strong> enthusiast from Belgium.
-          <span>👨🏼‍💻 🇧🇪</span> <br /><br />
-          I have a genuine love for coding and design that has been with me
-          since my early days as a student, and it continues to drive me forward
-          in my developer journey. <span>🚀</span> <br /><br />
-          Along the way, I discovered the joys of <strong>freelancing</strong>,
-          which has allowed me to constantly expand my skills and delve into
-          exciting new technologies that benefit my <strong>clients</strong>.
-          <span>👏🏻</span> <br /><br />
-          I'm here to <strong>connect</strong> with all of you, whether you have
-          questions, ideas, or potential <strong>collaborations</strong> in
-          mind. Don't hesitate to reach out and let's have a chat. Together, we
-          can bring your visions to life! <span>💭</span> <br /><br />
-          Looking forward to hearing from you! <span>💌</span>
-        </p>
-      </header>
+				<p
+					class="text-md lg:text-lg lg:ml-5 mt-10 text-balance leading-loose"
+				>
+					I'm Kevin, a <strong>Full-stack Developer</strong> and
+					<strong>Laravel / Vue.js</strong> enthusiast from Belgium.
+					<span>👨🏼‍💻 🇧🇪</span> <br /><br />
+					I have a genuine love for coding and design that has been
+					with me since my early days as a student, and it continues
+					to drive me forward in my developer journey.
+					<span>🚀</span> <br /><br />
+					Along the way, I discovered the joys of
+					<strong>freelancing</strong>, which has allowed me to
+					constantly expand my skills and delve into exciting new
+					technologies that benefit my <strong>clients</strong>.
+					<span>👏🏻</span> <br /><br />
+					I'm here to <strong>connect</strong> with all of you,
+					whether you have questions, ideas, or potential
+					<strong>collaborations</strong> in mind. Don't hesitate to
+					reach out and let's have a chat. Together, we can bring your
+					visions to life! <span>💭</span> <br /><br />
+					Looking forward to hearing from you! <span>💌</span>
+				</p>
+			</header>
 
-      <div
-        class="flex items-center xl:justify-center flex-1 w-full order-1 xl:order-2 mb-10"
-      >
-        <img
-          src="/img/logo.svg"
-          :alt="appConfig.title + ' Logo'"
-          ref="logo"
-          class="w-[100px] xl:w-4/12"
-        />
-      </div>
-    </div>
-  </div>
+			<div
+				class="flex items-center xl:justify-center order-1 xl:order-2 flex-1 pt-20"
+			>
+				<spline-viewer
+					v-pre
+					url="https://prod.spline.design/DXVadsd0zl3Qf9ih/scene.splinecode"
+					class="h-full hidden xl:block"
+				></spline-viewer>
+				<img
+					src="/img/logo.svg"
+					:alt="appConfig.title + ' Logo'"
+					class="w-[75px] xl:hidden mb-10"
+				/>
+			</div>
+		</div>
+	</div>
 </template>
 
 <style lang="scss">
 span {
-  @apply text-xl;
+	@apply text-xl;
 }
 </style>
